b5496f77c0eceb855efb8678258f2d307b97848e
sunyandi <sunyd@inspur.com>
2018-01-18 19:54:02 +0800

    Replace 'assertFalse(a in b)' with 'assertNotIn(a, b)'
    Trivial fix.
    
    Change-Id: I878e6ea821052066b5ad457e7665229bc987ce69

--- fixtures/tests/_fixtures/test_packagepath.py
+++ fixtures/tests/_fixtures/test_packagepath.py
@@ -27,10 +27,10 @@ class TestPackagePathEntry(testtools.TestCase):
     def test_adds_missing_to_end_package_path(self):
         uniquedir = self.useFixture(TempDir()).path
         fixture = PackagePathEntry('fixtures', uniquedir)
-        self.assertFalse(uniquedir in fixtures.__path__)
+        self.assertNotIn(uniquedir, fixtures.__path__)
         with fixture:
             self.assertTrue(uniquedir in fixtures.__path__)
-        self.assertFalse(uniquedir in fixtures.__path__)
+        self.assertNotIn(uniquedir, fixtures.__path__)
 
     def test_doesnt_alter_existing_entry(self):
         existingdir = fixtures.__path__[0]
